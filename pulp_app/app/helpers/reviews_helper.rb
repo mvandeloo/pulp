60e97ecdb95c6869bd9a64b644d1abe2923dba7d
mvandeloo <martin.vandeloo@googlemail.com>
2013-10-14 23:37:13 +0100

    img path changed

--- pulp_app/app/helpers/reviews_helper.rb
+++ pulp_app/app/helpers/reviews_helper.rb
@@ -3,7 +3,7 @@ module ReviewsHelper
 	def stars_for_rating(rating)
  	stars = ' '
  	rating.times do
- 	stars << image_tag('app/assets/images/star-on-big.png')
+ 	stars << image_tag('star-on-big.png')
 	end
 	stars.html_safe
 	end
